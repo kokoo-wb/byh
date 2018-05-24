32268442fa7837a0dec9038377733cb5810c7ac9
ethan-zhs <839710683@qq.com>
2018-05-25 02:27:06 +0800

    activity page update

--- src/components/personal/activity/LotteryDrawRecord.jsx
+++ src/components/personal/activity/LotteryDrawRecord.jsx
@@ -4,12 +4,32 @@ import { Button } from 'antd-mobile'
 import './style.less'
 
 class LotteryDrawRecord extends Component {
-	render() {
-		
-		return (
-            <div>aaaaa</div>
-		)
-	}
+    render() {
+
+        return (
+            <div className="lottery-draw-record">
+                <ul>
+                    <li>
+                        <div className="record-msg">
+                            <p>抽奖名称</p>
+                            <p>时间：17/09/09 08:00</p>
+                            <p>结果：谢谢参与</p>
+                            <p>消耗：-10积分</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="record-msg">
+                            <p>抽奖名称</p>
+                            <p>时间：17/09/09 08:00</p>
+                            <p>结果：谢谢参与</p>
+                            <p>消耗：-10积分</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default LotteryDrawRecord
--- src/components/personal/commodity/Commodity.jsx
+++ src/components/personal/commodity/Commodity.jsx
@@ -1,72 +1,130 @@
 import React, { Component } from 'react'
-import { Button } from 'antd-mobile'
+import { Carousel, WingBlank } from 'antd-mobile'
 
 import './style.less'
 
 class Commodity extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: ['1', '2', '3'],
+            imgHeight: 176
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+            });
+        }, 100);
+    }
+
     render() {
 
         return (
-            <div className="my-weapon-container">
-                <ul>
-                    <li>
-                        <div className="weapon-img">
-                            <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
-                        </div>
-                        <div className="weapon-msg">
-                            <p>武器名称</p>
-                            <p>兑换积分：7777</p>
-                            <p>所剩数量：809</p>
-                            <p>商品条件：这个是文字编辑内容</p>
-                            <p>2017/09/09 00:00 到期</p>
-                        </div>
-                        <a className="use-btn">立即使用</a>
-                    </li>
+            <div className="commodity-container">
+                <div style={{ borderBottom: '1px solid #e8e8e8' }}>
+                    <WingBlank>
+                        <Carousel
+                            autoplay={true}
+                            infinite
+                            beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                            afterChange={index => console.log('slide to', index)}
+                        >
+                            {this.state.data.map(val => (
+                                <a
+                                    key={val}
+                                    href="http://www.alipay.com"
+                                    style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                                >
+                                    <img
+                                        src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                                        alt=""
+                                        style={{ width: '100%', verticalAlign: 'top' }}
+                                        onLoad={() => {
+                                            // fire window resize event to change height
+                                            window.dispatchEvent(new Event('resize'));
+                                            this.setState({ imgHeight: 'auto' });
+                                        }}
+                                    />
+                                </a>
+                            ))}
+                        </Carousel>
+                    </WingBlank>
+                </div>
 
-                    <li>
-                        <div className="weapon-img">
-                            <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
-                        </div>
-                        <div className="weapon-msg">
-                            <p>武器名称</p>
-                            <p>兑换积分：7777</p>
-                            <p>所剩数量：809</p>
-                            <p>商品条件：这个是文字编辑内容</p>
-                            <p>2017/09/09 00:00 到期</p>
-                        </div>
-                        <a className="use-btn-disable">立即使用</a>
-                    </li>
 
-                    <li>
-                        <div className="weapon-img">
-                            <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
-                        </div>
-                        <div className="weapon-msg">
-                            <p>武器名称</p>
-                            <p>兑换积分：7777</p>
-                            <p>所剩数量：809</p>
-                            <p>商品条件：这个是文字编辑内容</p>
-                            <p>2017/09/09 00:00 到期</p>
-                        </div>
-                        <a className="use-btn">立即使用</a>
-                    </li>
+                <div className="menu-choose">
+                    <div className="menu-choose-content">
+                        <a className="active">交易工具</a>
+                        <a>会员提升</a>
+                        <a>其他</a>
+                    </div>
+                </div>
 
-                    <li>
-                        <div className="weapon-img">
-                            <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
-                        </div>
-                        <div className="weapon-msg">
-                            <p>武器名称</p>
-                            <p>兑换积分：7777</p>
-                            <p>所剩数量：809</p>
-                            <p>商品条件：这个是文字编辑内容</p>
-                            <p>2017/09/09 00:00 到期</p>
-                        </div>
-                        <a className="use-btn">立即使用</a>
-                    </li>
-                </ul>
+                <div className="commodity-weapon">
+                    <ul>
+                        <li>
+                            <div className="weapon-img">
+                                <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
+                            </div>
+                            <div className="weapon-msg">
+                                <p>武器名称</p>
+                                <p>兑换积分：7777</p>
+                                <p>所剩数量：809</p>
+                                <p>商品条件：这个是文字编辑内容</p>
+                                <p>2017/09/09 00:00 到期</p>
+                            </div>
+                            <a className="use-btn">立即使用</a>
+                        </li>
 
-                <div className="bonus-point">当前积分：2423</div>
+                        <li>
+                            <div className="weapon-img">
+                                <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
+                            </div>
+                            <div className="weapon-msg">
+                                <p>武器名称</p>
+                                <p>兑换积分：7777</p>
+                                <p>所剩数量：809</p>
+                                <p>商品条件：这个是文字编辑内容</p>
+                                <p>2017/09/09 00:00 到期</p>
+                            </div>
+                            <a className="use-btn-disable">立即使用</a>
+                        </li>
+
+                        <li>
+                            <div className="weapon-img">
+                                <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
+                            </div>
+                            <div className="weapon-msg">
+                                <p>武器名称</p>
+                                <p>兑换积分：7777</p>
+                                <p>所剩数量：809</p>
+                                <p>商品条件：这个是文字编辑内容</p>
+                                <p>2017/09/09 00:00 到期</p>
+                            </div>
+                            <a className="use-btn">立即使用</a>
+                        </li>
+
+                        <li>
+                            <div className="weapon-img">
+                                <img src="http://img1.imgtn.bdimg.com/it/u=2511394165,1594141098&fm=27&gp=0.jpg" />
+                            </div>
+                            <div className="weapon-msg">
+                                <p>武器名称</p>
+                                <p>兑换积分：7777</p>
+                                <p>所剩数量：809</p>
+                                <p>商品条件：这个是文字编辑内容</p>
+                                <p>2017/09/09 00:00 到期</p>
+                            </div>
+                            <a className="use-btn">立即使用</a>
+                        </li>
+                    </ul>
+
+                    <div className="bonus-point">当前积分：2423</div>
+                </div>
             </div>
         )
     }
